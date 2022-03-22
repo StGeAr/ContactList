b8df497d28adb13dc573883747de01d9e2c87c21
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-22 13:12:44 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Герман Ставицкий on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
